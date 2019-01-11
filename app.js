992e0beff9c174a5795511a74f676db30b64dec6
Ana Walton <awalton3@nd.edu>
2019-01-10 22:34:27 -0500

    added additional navbar animations for accessibility

--- app.js
+++ app.js
@@ -5,3 +5,14 @@
 import {MDCTopAppBar} from '@material/top-app-bar/index';
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+//navbar scroll animation - from transparent to filled
+$(document).ready(function() {
+  $(function() {
+    $(document).scroll(function() {
+      var $nav = $(".navbar");
+      var $welcome = $(".welcome");
+      $nav.toggleClass('navbar--scrolled', $(this).scrollTop() > $welcome.height() - 200);
+    });
+  });
+})
